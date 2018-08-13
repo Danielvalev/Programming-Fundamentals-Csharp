a5cebb59d0105748ed0cca3848dddb6c7efc9b86
Daniel Valev <danielvalev89@gmail.com>
2018-08-13 17:34:04 +0300

    added C# data types and variables

---
+++ 03. C# Data Types And Variables/Exercises/09. Reversed chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Reversed_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input1 = char.Parse(Console.ReadLine());
+            char input2 = char.Parse(Console.ReadLine());
+            char input3 = char.Parse(Console.ReadLine());
+
+            string word = "" + input1 + input2 + input3;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+        }
+    }
+}
---
+++ 03. C# Data Types And Variables/Exercises/13. Vowel or Digit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _13._Vowel_or_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+
+            if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u' || symbol == 'y')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if ((int)symbol >= 48 && (int)symbol <= 57)
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
