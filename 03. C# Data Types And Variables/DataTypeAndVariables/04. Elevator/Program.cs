946927b80019cb103636fc5953d4327fa8e1c2d4
Daniel Valev <danielvalev89@gmail.com>
2018-08-13 17:25:10 +0300

    added C# data types and variables

---
+++ 03. C# Data Types And Variables/DataTypeAndVariables/04. Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int personNumber = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int cources = (int)Math.Ceiling((double)personNumber / capacity);
+
+            Console.WriteLine(cources);
+
+        }
+    }
+}
---
+++ 03. C# Data Types And Variables/DataTypeAndVariables/06. Triples of Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Triples_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char letter = (char)('a' + n);
+
+            for (char i = 'a'; i < letter; i++)
+            {
+                for (char j = 'a'; j < letter; j++)
+                {
+                    for (char k = 'a'; k < letter; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
