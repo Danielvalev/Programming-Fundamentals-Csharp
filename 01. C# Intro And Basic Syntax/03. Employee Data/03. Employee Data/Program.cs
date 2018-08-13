2a843442ad39a0e52f72d1e351fc7d8354867a0f
Daniel Valev <danielvalev89@gmail.com>
2018-08-13 16:41:19 +0300

    added C# intro and basic syntax

---
+++ 01. C# Intro And Basic Syntax/03. Employee Data/03. Employee Data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeId = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine($"Employee ID: {employeeId:D8}");
+            Console.WriteLine($"Salary: {salary:F2}");
+        }
+    }
+}
---
+++ 01. C# Intro And Basic Syntax/04. Beverage Labels/04. Beverage Labels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double totalEnergy = (volume * energy) / 100.0;
+            double totalSugar = (volume * sugar) / 100;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
+    
+
+
+        }
+    }
+}
