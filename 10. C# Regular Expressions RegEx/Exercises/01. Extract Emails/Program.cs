0126890bed3c14cb8c652b41e86011882ed4904f
Daniel Valev <danielvalev89@gmail.com>
2018-08-13 16:51:45 +0300

    Add files via upload

---
+++ 10. C# Regular Expressions RegEx/Exercises/01. Extract Emails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s)[A-Za-z0-9]+([.\-_]?[A-Za-z0-9]+)*@[A-Za-z]+(\-[A-Za-z]+)*(\.[A-Za-z]+)+";
+
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
---
+++ 10. C# Regular Expressions RegEx/Exercises/02. Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Extract_Sentences_by_Keyword
+{
+    class Program
+    {
+        public static object RemoveEmptyEntries { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string searchWord = Console.ReadLine();
+
+            string pattern = $@"\b{searchWord}\b";
+
+            Regex regex = new Regex(pattern);
+
+            string[] words = Console.ReadLine().Split(new[] {'.', '?', '!'}, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+
+            foreach (string word in words)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
