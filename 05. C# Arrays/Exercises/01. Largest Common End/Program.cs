2a923f9f937447d9fc81721c30b4c539a4cf182c
Daniel Valev <danielvalev89@gmail.com>
2018-08-14 11:06:47 +0300

    added C# arrays

---
+++ 05. C# Arrays/Exercises/01. Largest Common End/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Largest_Common_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string[] secondInput = Console.ReadLine().Split();
+
+            int minLength = Math.Min(firstInput.Length, secondInput.Length);
+
+            int countLeft = 0;
+            int countRight = 0;
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (firstInput[i] == secondInput[i])
+                {
+                    countLeft++;
+                }
+
+                if (firstInput[firstInput.Length - 1 - i] == secondInput[secondInput.Length - 1 - i])
+                {
+                    countRight++;
+                }
+
+            }
+            Console.WriteLine(Math.Max(countLeft, countRight));
+        }
+    }
+}
---
+++ 05. C# Arrays/Exercises/03. Fold and Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            int k = numbersArr.Length / 4;
+
+            int[] upperArr = new int[k * 2];
+
+            int[] lowerArr = new int[k * 2];
+
+            for (int i = 0; i < k; i++)
+            {
+                upperArr[i] = numbersArr[k - i - 1];
+                upperArr[]
+            }
+        }
+    }
+}
