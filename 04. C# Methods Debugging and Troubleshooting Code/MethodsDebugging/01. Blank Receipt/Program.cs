c0b2cc66c2f0f62bc3799a0a7be8b448a594256d
Daniel Valev <danielvalev89@gmail.com>
2018-08-14 10:32:23 +0300

    added C# methods debugging and troubleshooting

---
+++ 04. C# Methods Debugging and Troubleshooting Code/MethodsDebugging/01. Blank Receipt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Blank_Receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printReceipt();
+        }
+
+        static void printReceipt()
+        {
+            printReceiptHeader();
+            printReceiptBody();
+            printReceiptFooter();
+        }
+
+        static void printReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT\n------------------------------");
+        }
+
+        static void printReceiptBody()
+        {
+            Console.WriteLine("Charged to____________________\nReceived by___________________");
+        }
+
+        static void printReceiptFooter()
+        {
+            Console.WriteLine("------------------------------\n© SoftUni");
+        }
+    }
+}
---
+++ 04. C# Methods Debugging and Troubleshooting Code/MethodsDebugging/05. Temperature Conversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Temperature_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printReceipt();
+        }
+
+        static void printReceipt()
+        {
+            printReceiptHeader();
+            printReceiptBody();
+            printReceiptFooter();
+        }
+
+        static void printReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT\n------------------------------");
+        }
+
+        static void printReceiptBody()
+        {
+            Console.WriteLine("Charged to____________________\nReceived by___________________");
+        }
+
+        static void printReceiptFooter()
+        {
+            Console.WriteLine("------------------------------\n© SoftUni");
+        }
+    }
+}
