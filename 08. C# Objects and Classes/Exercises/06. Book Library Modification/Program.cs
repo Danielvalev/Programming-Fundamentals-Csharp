c75f4be5972bfe2a9963d5cc04a9c1a241254b05
Daniel Valev <danielvalev89@gmail.com>
2018-08-14 11:51:43 +0300

    added C# objects and classes

---
+++ 08. C# Objects and Classes/Exercises/06. Book Library Modification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Book_Library_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
